ebd49ebc3122d58e8022ef6ee40f3691e9a7fa99
alnah005 <alnah005@umn.edu>
2020-12-01 00:44:12 -0600

    added agent and enviornment factories

---
+++ VIAYN/samples/factory.py
@@ -0,0 +1,47 @@
+from VIAYN.project_types import Agent, Environment,Dict
+from typing import Any
+
+class AgentFactory:
+    """
+    Creates different types of Agents Based on dictionary specified
+
+    List of acceptable configs:
+    'type': 'constant','random'
+    'vote': float (default = 0.0 or highest vote)
+    'prediction': ActionBet(bet = 0.0 or max moneys, prediction = [])
+
+    Parameters
+    ----------
+    spec: Dict
+        Information to initialize Agents
+
+    Returns
+    -------
+    Agent
+        created agent based on spec
+    """
+
+    def create(self,spec: Dict[str,Any]) -> Agent:
+        return Agent()
+
+class EnvFactory:
+    """
+    Creates different types of Environments Based on dictionary specified
+
+
+    List of acceptable configs:
+    TBD
+
+    Parameters
+    ----------
+    spec: Dict
+        Information to initialize Environments
+
+    Returns
+    -------
+    Envioronment
+        created environment based on spec
+    """
+
+    def create(self,spec: Dict[str,Any]) -> Environment:
+        return Environment()
