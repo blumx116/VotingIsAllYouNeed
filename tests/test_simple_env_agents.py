c4031dec580bc6066bc5925760872eec296526e4
alnah005 <alnah005@umn.edu>
2020-12-03 19:56:56 -0600

    started issue 7

---
+++ tests/test_simple_env_agents.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: Suhail.Alnahari
+# @Date:   2020-12-03 19:25:18
+# @Last Modified by:   Suhail.Alnahari
+# @Last Modified time: 2020-12-03 19:26:49
+
+
+from conftest import pytest, project_types
+
+
