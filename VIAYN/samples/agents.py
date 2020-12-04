e377e54fe280ac34135a5bfe8906482c34d2d54d
Carter Blum <blumx116@umn.edu>
2020-12-03 20:53:19 -0600

    intermediate commit : agents.py in progress

---
+++ VIAYN/samples/agents.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from VIAYN.project_types import Agent, StateType, ActionType, ActionBet
+
+class VotingMechanism(ABC):
+    @abstractmethod
+    def vote(self, state: StateType) -> float:
+        ...
+
+class StaticVotingMechanism(VotingMechanism):
+    def __init__(self,
+            constant_vote: float):
+        self.constant_vote: float = constant_vote
+
+    def vote(self, state: StateType) -> float:
+        return self.constant_vote
+
+class BettingMechanism(ABC):
+    @abstractmethod
+    def bet(self, state: StateType, action: ActionType, money: float) -> ActionBet:
+        ...
+
+class StaticBettingMechanism(BettingMechanism):
+    def __init__(self,
+            constant_bet: List[float],
+            constant_prediction: List[float],
+            valid_prediction_range: Tuple[float, float]):
+        self.constant_bet: List[float] = constant_bet
+        self.constant_prediction: List[float] = constant_prediction
+
+    def bet(self, state: StateType, action: ActionType, money: float) -> ActionBet:
+        return
+
+
+class StaticAgent(Agent):
+    def __init__(self,
+            bet_amount: List[float],
+            prediction: List[float],
+            vote_amount: float):
+
+        assert 0 <= bet_amount <= 1
+
+        self.bet_amount: List[float] = bet_amount
+        self.vote_amount: float = vote_amount
+        self.prediction: List[float] = prediction
+
+    def bet(self, state: StateType, action: ActionType, money: float) -> ActionBet:
+        return ActionBet(bet=self.bet_amount, prediction=self.prediction)
+
+    def vote(self, state: StateType) -> float:
+        return self.vote_amount
+
+class DumbAgentWithStaticVote
