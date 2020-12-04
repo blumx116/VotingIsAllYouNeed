757494adfba8b6be42ba652d0eb76b78704cadfc
Carter Blum <blumx116@umn.edu>
2020-12-04 17:07:14 -0600

    linting and types

--- VIAYN/samples/agents.py
+++ VIAYN/samples/agents.py
@@ -1,17 +1,19 @@
 from abc import ABC, abstractmethod
 from copy import copy
-from typing import List, Callable, Optional
+from typing import List, Callable, Optional, Generic
 
 import numpy as np
 
 from VIAYN.project_types import Agent, StateType, ActionType, ActionBet
 
-class VotingMechanism(ABC):
+
+class VotingMechanism(Generic[StateType], ABC):
     @abstractmethod
     def vote(self, state: StateType) -> float:
         ...
 
-class StaticVotingMechanism(VotingMechanism):
+
+class StaticVotingMechanism(Generic[StateType], VotingMechanism[StateType]):
     def __init__(self,
             constant_vote: float):
         self.constant_vote: float = constant_vote
@@ -19,10 +21,12 @@ class StaticVotingMechanism(VotingMechanism):
     def vote(self, state: StateType) -> float:
         return self.constant_vote
 
+
 class BettingMechanism(ABC):
     @abstractmethod
     def bet(self, state: StateType, action: ActionType, money: float) -> ActionBet:
         ...
+
 
 class StaticBettingMechanism(BettingMechanism):
     def __init__(self,
@@ -38,19 +42,19 @@ class StaticBettingMechanism(BettingMechanism):
         bet: List[float] = copy(self.constant_bet)
         return ActionBet(bet=bet, prediction=prediction)
 
+
 class UniformBettingMechanism(BettingMechanism):
     def __init__(self,
             constant_bet: List[float],
             tsteps_per_prediction: int,
-            min_possible_prediction: Callable[[], float],
-            max_possible_prediction: Callable[[], float],
-            random_seed: int = None):
+            min_possible_prediction: Callable[[int], float],
+            max_possible_prediction: Callable[[int], float],
+            random_seed: int):
         self.tsteps_per_prediction: int = tsteps_per_prediction
         self.constant_bet: List[float] = constant_bet
-        self.min_possible_prediction: Optional[Callable[[int], float]] = min_possible_prediction
-        self.max_possible_prediction: Optional[Callable[[int], float]] = max_possible_prediction
+        self.min_possible_prediction: Callable[[int], float] = min_possible_prediction
+        self.max_possible_prediction: Callable[[int], float] = max_possible_prediction
         self.random = np.random.RandomState(random_seed)
-
 
     def bet(self, state: StateType, action: ActionType, money: float) -> ActionBet:
         bet: List[float] = copy(self.constant_bet)
@@ -58,6 +62,7 @@ class UniformBettingMechanism(BettingMechanism):
             [self.random.uniform(low=self.min_possible_prediction(dt), high=self.max_possible_prediction(dt))
                 for dt in range(self.tsteps_per_prediction)]
         return ActionBet(bet=bet, prediction=prediction)
+
 
 class CompositeAgent(Agent):
     def __init__(self,
@@ -71,4 +76,3 @@ class CompositeAgent(Agent):
 
     def bet(self, state: StateType, action: ActionType, money: float) -> ActionBet:
         return self.betting_mechanism.bet(state, action, money)
-
