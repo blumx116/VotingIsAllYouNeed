242efcdd1a3b7661de14ab3c320658502c45e86c
Carter Blum <blumx116@umn.edu>
2020-12-04 22:24:41 -0600

    add select_action in train.py

--- VIAYN/train.py
+++ VIAYN/train.py
@@ -2,7 +2,7 @@
 # @Author: Carter.Blum
 # @Date:   2020-12-10 14:37:06
 # @Last Modified by:   Suhail.Alnahari
-# @Last Modified time: 2020-12-10 14:40:06
+# @Last Modified time: 2020-12-10 14:40:38
 from typing import List, Iterable, Dict, Tuple
 
 import numpy as np
