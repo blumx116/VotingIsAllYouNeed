f1519018bf62d7568dec998712f26d049724b961
Carter Blum <blumx116@umn.edu>
2020-12-04 22:51:35 -0600

    merge in shortened variable names for agents.py

--- VIAYN/samples/agents.py
+++ VIAYN/samples/agents.py
@@ -7,45 +7,45 @@ import numpy as np
 from VIAYN.project_types import Agent, A, S, ActionBet
 
 
-class VotingMechanism(Generic[StateType], ABC):
+class VotingMechanism(Generic[S], ABC):
     @abstractmethod
     def vote(self, state: S) -> float:
         ...
 
 
-class StaticVotingMechanism(Generic[StateType], VotingMechanism[StateType]):
+class StaticVotingMechanism(Generic[S], VotingMechanism[S]):
     def __init__(self,
             constant_vote: float):
         self.constant_vote: float = constant_vote
 
-    def vote(self, state: StateType) -> float:
+    def vote(self, state: S) -> float:
         return self.constant_vote
 
 
-class BetSelectionMechanism(Generic[StateType, ActionType], ABC):
+class BetSelectionMechanism(Generic[S, A], ABC):
     @abstractmethod
-    def select_bet_amount(self, state: StateType, action: ActionType, money: float) -> List[float]:
+    def select_bet_amount(self, state: S, action: A, money: float) -> List[float]:
         ...
 
 
-class StaticBetSelectionMech(Generic[StateType, ActionType], BetSelectionMechanism):
+class StaticBetSelectionMech(Generic[S, A], BetSelectionMechanism):
     def __init__(self,
             constant_bet: List[float]):
         for bet in constant_bet:
             assert 0 <= bet <= 1
         self.constant_bet: List[float] = constant_bet
 
-    def select_bet_amount(self, state: StateType, action: ActionType, money: float) -> List[float]:
+    def select_bet_amount(self, state: S, action: A, money: float) -> List[float]:
         return copy(self.constant_bet)
 
 
-class PredictionSelectionMechanism(Generic[StateType, ActionType], ABC):
+class PredictionSelectionMechanism(Generic[S, A], ABC):
     @abstractmethod
-    def select_prediction(self, state: StateType, action: ActionType, money: float) -> List[float]:
+    def select_prediction(self, state: S, action: A, money: float) -> List[float]:
         ...
 
 
-class RNGUniforPredSelectionMech(Generic[StateType, ActionType], PredictionSelectionMechanism[StateType, ActionType]):
+class RNGUniforPredSelectionMech(Generic[S, A], PredictionSelectionMechanism[S, A]):
     def __init__(self,
             tsteps_per_prediction: int,
             min_possible_prediction: Callable[[int], float],
@@ -56,35 +56,35 @@ class RNGUniforPredSelectionMech(Generic[StateType, ActionType], PredictionSelec
         self.max_possible_prediction: Callable[[int], float] = max_possible_prediction
         self.random = np.random.RandomState(random_seed)
 
-    def select_prediction(self, state: StateType, action: ActionType, money: float) -> List[float]:
+    def select_prediction(self, state: S, action: A, money: float) -> List[float]:
         return [self.random.uniform(low=self.min_possible_prediction(dt), high=self.max_possible_prediction(dt))
          for dt in range(self.tsteps_per_prediction)]
 
 
-class StaticPredSelectionMech(Generic[StateType, ActionType], PredictionSelectionMechanism[StateType, ActionType]):
+class StaticPredSelectionMech(Generic[S, A], PredictionSelectionMechanism[S, A]):
     def __init__(self,
             constant_prediction: List[float]):
         self.constant_prediction: List[float] = constant_prediction
 
-    def select_prediction(self, state: StateType, action: ActionType, money: float) -> List[float]:
+    def select_prediction(self, state: S, action: A, money: float) -> List[float]:
         return copy(self.constant_prediction)
 
-class BettingMechanism(Generic[StateType, ActionType], ABC):
+class BettingMechanism(Generic[S, A], ABC):
     @abstractmethod
-    def bet(self, state: StateType, action: ActionType, money: float) -> ActionBet:
+    def bet(self, state: S, action: A, money: float) -> ActionBet:
         ...
 
 
 class CompositeBettingMechanism(BettingMechanism):
     def __init__(self,
-            bet_selection: BetSelectionMechanism[StateType, ActionType],
-            prediction_selection: PredictionSelectionMechanism[StateType, ActionType]):
-        self.bet_selection_mech: BetSelectionMechanism[StateType, ActionType] = \
+            bet_selection: BetSelectionMechanism[S, A],
+            prediction_selection: PredictionSelectionMechanism[S, A]):
+        self.bet_selection_mech: BetSelectionMechanism[S, A] = \
             bet_selection
-        self.prediction_selection_mech: PredictionSelectionMechanism[StateType, ActionType] = \
+        self.prediction_selection_mech: PredictionSelectionMechanism[S, A] = \
             prediction_selection
 
-    def bet(self, state: StateType, action: ActionType, money: float) -> ActionBet:
+    def bet(self, state: S, action: A, money: float) -> ActionBet:
         return ActionBet(
             bet=self.bet_selection_mech.select_bet_amount(state, action, money),
             prediction=self.prediction_selection_mech.select_prediction(state, action, money))
@@ -99,7 +99,7 @@ class StaticBettingMechanism(BettingMechanism):
         self.constant_bet: List[float] = constant_bet
         self.constant_prediction: List[float] = constant_prediction
 
-    def bet(self, state: StateType, action: ActionType, money: float) -> ActionBet:
+    def bet(self, state: S, action: A, money: float) -> ActionBet:
         prediction: List[float] = copy(self.constant_prediction)
         bet: List[float] = copy(self.constant_bet)
         return ActionBet(bet=bet, prediction=prediction)
@@ -118,7 +118,7 @@ class UniformBettingMechanism(BettingMechanism):
         self.max_possible_prediction: Callable[[int], float] = max_possible_prediction
         self.random = np.random.RandomState(random_seed)
 
-    def bet(self, state: StateType, action: ActionType, money: float) -> ActionBet:
+    def bet(self, state: S, action: A, money: float) -> ActionBet:
         bet: List[float] = copy(self.constant_bet)
         prediction: List[float] = \
             [self.random.uniform(low=self.min_possible_prediction(dt), high=self.max_possible_prediction(dt))
@@ -133,8 +133,8 @@ class CompositeAgent(Agent):
         self.betting_mechanism: BettingMechanism = betting_mechanism
         self.voting_mechanism: VotingMechanism = voting_mechanism
 
-    def vote(self, state: StateType) -> float:
+    def vote(self, state: S) -> float:
         return self.voting_mechanism.vote(state)
 
-    def bet(self, state: StateType, action: ActionType, money: float) -> ActionBet:
+    def bet(self, state: S, action: A, money: float) -> ActionBet:
         return self.betting_mechanism.bet(state, action, money)
