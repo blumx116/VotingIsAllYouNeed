f7ccc06f1550710e7bf912fe0d7ae722098a0517
Carter Blum <blumx116@umn.edu>
2020-12-07 22:35:51 -0600

    Configuration should also have already been added??

--- VIAYN/project_types.py
+++ VIAYN/project_types.py
@@ -2,7 +2,7 @@
 # @Author: Carter.Blum
 # @Date:   2020-11-27 20:48:03
 # @Last Modified by:   Suhail.Alnahari
-# @Last Modified time: 2020-12-10 14:51:55
+# @Last Modified time: 2020-12-10 14:56:01
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
