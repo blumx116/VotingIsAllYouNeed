62949b148c5c1023a920e79ad8611b3a78597b06
Carter Blum <blumx116@umn.edu>
2020-12-09 21:05:44 -0600

    added ThompsonPolicy skeleton

--- VIAYN/samples/policy.py
+++ VIAYN/samples/policy.py
@@ -2,6 +2,7 @@ from typing import Generic, List, Dict, Optional
 
 from VIAYN.project_types import PolicyConfiguration, A, B, S, WeightedBet
 from VIAYN.utils import weighted_mean, argmax
+from VIAYN.DiscreteDistribution import  DiscreteDistribution
 
 
 class GreedyPolicyConfiguration(Generic[A, S], PolicyConfiguration[A, float, S]):
@@ -22,3 +23,20 @@ class GreedyPolicyConfiguration(Generic[A, S], PolicyConfiguration[A, float, S])
             aggregate_bets: Dict[A, float]) -> Dict[A, float]:
         chosen_action: A = GreedyPolicyConfiguration.select_action(aggregate_bets)
         return {action: (1. if action == chosen_action else 0.) for action in aggregate_bets.keys()}
+
+
+class ThompsonPolicyConfiguration(Generic[A, S], PolicyConfiguration[A, DiscreteDistribution, S]):
+    @staticmethod
+    def aggregate_bets(
+            predictions: Dict[A, List[WeightedBet[A, S]]]) -> Dict[A, DiscreteDistribution]:
+        ...
+
+    @staticmethod
+    def select_action(
+            aggregate_bets: Dict[A, DiscreteDistribution]) -> A:
+        ...
+
+    @staticmethod
+    def action_probabilities(
+            aggregate_bets: Dict[A, DiscreteDistribution]) -> Dict[A, float]:
+        ...
