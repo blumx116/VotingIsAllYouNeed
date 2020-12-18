886290d2889cba01c67920c4a41ce0fcf89fd802
alnah005 <alnah005@umn.edu>
2020-12-18 17:21:50 -0600

    added docs for simple env agents and utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,23 +1,32 @@
 # -*- coding: utf-8 -*-
-# @Author: Suhail.Alnahari
-# @Date:   2020-12-10 15:47:28
-# @Last Modified by:   Suhail.Alnahari
-# @Last Modified time: 2020-12-11 22:28:21
+"""
+Created on 2020-12-10 15:47:28
+@author: suhail
+
+This file tests general utils we are using
+"""
+
+# standard library
 from typing import List
-from tests.conftest import floatIsEqual
+
+# 3rd party packages
 import pytest
+import numpy as np
+
+# local source
+from tests.conftest import floatIsEqual
 import VIAYN.project_types as P
 import VIAYN.samples.factory as fac
 import VIAYN.samples.vote_ranges as vote_range
-import numpy as np
 import VIAYN.utils as U
 
 
 @pytest.mark.parametrize("dict1,dict2,expected", [
-    ({'a1':1,'a2':2},{},{'a1':1,'a2':2}),
-    ({},{'a1':1,'a2':2},{'a1':1,'a2':2}),
-    ({'a1':1,'a2':2},{'a3':1,'a4':2},{'a1':1,'a2':2,'a3':1,'a4':2}),
-    ({},{},{}),
+    ({'a1':1,'a2':2},{},{'a1':1,'a2':2}), # identical dicts
+    ({},{'a1':1,'a2':2},{'a1':1,'a2':2}), # one empty dict
+    ({'a1':1,'a2':2},{},{'a1':1,'a2':2}), # the other is empty
+    ({'a1':1,'a2':2},{'a3':1,'a4':2},{'a1':1,'a2':2,'a3':1,'a4':2}), # unique dicts
+    ({},{},{}), # dicts are all empty
 ])
 def test_add_dictionaries(dict1,dict2,expected):
     res : dict = U.add_dictionaries(dict1,dict2)
@@ -27,6 +36,8 @@ def test_add_dictionaries(dict1,dict2,expected):
 
 
 @pytest.mark.parametrize("bets,predictions,moneys,expected", [
+    # random configurations of a three agent system with identical money start
+    # using one time-step predictions 
     (
         [[0],[0.5],[0.2]],
         [[2],[4],[5]],
@@ -45,6 +56,11 @@ def test_add_dictionaries(dict1,dict2,expected):
         [2,2,2],
         [(0.8/1.4)+((0.6*4)/1.4)+(2./1.4)]
     ),
+    # random configurations of a three agent system with identical money end
+    # using one time-step predictions
+
+    # random configurations of a three agent system with identical money start
+    # using two time-step predictions
     (
         [[0.2,0.2],[0.3,0.3],[0.2,0.2]],
         [[2,2],[4,4],[5,5]],
@@ -57,6 +73,11 @@ def test_add_dictionaries(dict1,dict2,expected):
         [2,2,2],
         [(0.8/1.4)+((0.6*4)/1.4)+(2./1.4), (0.8/1.4)+((0.6*4)/1.4)+(2./1.4)]
     ),
+    # random configurations of a three agent system with identical money end
+    # using two time-step predictions
+    
+    # random configurations of a three agent system with varying money start
+    # using one time-step predictions
     (
         [[0],[0.5],[0.2]],
         [[2],[4],[5]],
@@ -75,14 +96,31 @@ def test_add_dictionaries(dict1,dict2,expected):
         [2,3,4],
         [(0.8/2.1)+((0.9*4)/2.1)+(4./2.1)]
     ),
+    # random configurations of a three agent system with varying money end
+    # using one time-step predictions
+
+    # random configurations of a three agent system with varying money start
+    # using two time-step predictions
     (
         [[0.2,0.2],[0.3,0.3],[0.2,0.2]],
         [[2,2],[4,4],[5,5]],
         [2,3,4],
         [(0.8/2.1)+((0.9*4)/2.1)+(4./2.1), (0.8/2.1)+((0.9*4)/2.1)+(4./2.1)]
     )
+    # random configurations of a three agent system with varying money end
+    # using two time-step predictions
 ])
 def test_weighted_mean(bets,predictions,moneys,expected,gen_weighted_bet):
+    """
+    Checks that given a set of agent [bets] and [predictions], the weighted
+    mean of them is equal to sum b_it p_it m_i / sum b_i m_i per time-step
+    
+    [bets] b_it
+    [predictions] p_it
+    [moneys] m_i
+    The other parameter is a test fixture to help create
+    objects easier.
+    """
     weightedBets : List[P.WeightedBet] = []
     for i in range(len(bets)):
         print(bets[i],predictions[i],moneys[i])
@@ -96,42 +134,49 @@ def test_weighted_mean(bets,predictions,moneys,expected,gen_weighted_bet):
 
 
 @pytest.mark.parametrize("bets,predictions,moneys,expected", [
+    # zero/zero
     (
         [[0]],
         [[0]],
         [2],
         [0]
     ),
+    # bets length is not equal to prediction length
     (
         [[0,2,3]],
         [[0]],
         [2],
         [0]
     ),
+    # prediction is empty
     (
         [[0,2,3]],
         [[]],
         [2],
         [0]
     ),
+    # no money
     (
         [[1,2,3]],
         [[3,2,3]],
         [],
         [0]
     ),
+    # empty bets and predictions
     (
         [],
         [],
         [2],
         []
     ),
+    # varying lengths per agent
     (
         [[1,2,3],[1,2],[1,2,3]],
         [[1,2,3],[1,2],[1,2,3]],
         [2,3,4],
         [0,2,3]
     ),
+    # varying lengths per agent
     (
         [[1,2],[1,2],[1,2,3]],
         [[1,2],[1,2],[1,2,3]],
@@ -140,6 +185,17 @@ def test_weighted_mean(bets,predictions,moneys,expected,gen_weighted_bet):
     )
 ])
 def test_weighted_mean_shouldFail(bets,predictions,moneys,expected,gen_weighted_bet):
+    """
+    Checks that given a set of agent [bets] and [predictions], the weighted
+    mean of them is equal to sum b_it p_it m_i / sum b_i m_i per time-step
+    cannot be calculated given the parameters input
+    
+    [bets] b_it
+    [predictions] p_it
+    [moneys] m_i
+    The other parameter is a test fixture to help create
+    objects easier.
+    """
     try:
         weightedBets : List[P.WeightedBet] = []
         for i in range(len(bets)):
@@ -155,13 +211,13 @@ def test_weighted_mean_shouldFail(bets,predictions,moneys,expected,gen_weighted_
 
 
 @pytest.mark.parametrize("arr,vals,expected", [
-    ([0,1,2,3,4,5],[0,0,0,0,0,100],5),
-    ([5,4,3,2,1,0],[0,0,0,0,0,100],5),
-    ([0,1,2,3,4,5],[0,10,100,1000,10000,100000],5),
-    ([5,4,3,2,1,0],[100000,10000,1000,100,10,0],0),
-    ([],[],None),
-    ([0,1,2,3,4],[1,1,1,1,1],0), # document this behaviour
-    ([1,1,1,1,1,1],[7,0,7,7,7,7,7,7],1), # and this
+    ([0,1,2,3,4,5],[0,0,0,0,0,100],5), # last item is the maximum
+    ([5,4,3,2,1,0],[0,0,0,0,0,100],5), # first item is the maximum
+    ([0,1,2,3,4,5],[0,10,100,1000,10000,100000],5), # varying values
+    ([5,4,3,2,1,0],[100000,10000,1000,100,10,0],0), # order doesn't matter
+    ([],[],None), # empty values
+    ([0,1,2,3,4],[1,1,1,1,1],0), # first item with identical value is returned #TODO: document this behavior
+    ([1,1,1,1,1,1],[7,0,7,7,7,7,7,7],1), # items with the same value return the same thing
 ])
 def test_argmax(arr,vals,expected):
     assert(U.argmax(arr,lambda x: vals[x]) == expected)
