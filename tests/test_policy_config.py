9b76f1a4bd2fc1b224a8ec04c65e9ffe3b85f0e2
alnah005 <alnah005@umn.edu>
2020-12-18 16:44:45 -0600

    added docs for policy config tests

--- tests/test_policy_config.py
+++ tests/test_policy_config.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
-# @Author: Suhail.Alnahari
-# @Date:   2020-12-06 18:09:26
-# @Last Modified by:   Suhail.Alnahari
-# @Last Modified time: 2020-12-11 23:13:54
+"""
+Created on 2020-12-06 18:09:26
+@author: suhail
 
+This file tests policy configurations
+"""
+
+# standard library
 from typing import List
-from tests.conftest import floatIsEqual
+
+# 3rd party packages
 import pytest
+
+# local source
+from tests.conftest import floatIsEqual
 import VIAYN.project_types as P
 import VIAYN.samples.factory as fac
 import VIAYN.samples.vote_ranges as vote_range
@@ -16,6 +23,7 @@ import VIAYN.utils as U
 
 
 @pytest.mark.parametrize("bets,predictions,moneys,expected", [
+    # varying bets and predictions for a three agent system over one time-step start
     (
         [[0],[0.5],[0.2]],
         [[2],[4],[5]],
@@ -34,6 +42,8 @@ import VIAYN.utils as U
         [2,2,2],
         [(0.8/1.4)+((0.6*4)/1.4)+(2./1.4)]
     ),
+    # varying bets and predictions for a three agent system over one time-step end
+    # varying bets and predictions for a three agent system over two time-steps start
     (
         [[0.2,0.2],[0.3,0.3],[0.2,0.2]],
         [[2,2],[4,4],[5,5]],
@@ -46,6 +56,8 @@ import VIAYN.utils as U
         [2,2,2],
         [(0.8/1.4)+((0.6*4)/1.4)+(2./1.4), (0.8/1.4)+((0.6*4)/1.4)+(2./1.4)]
     ),
+    # varying bets and predictions for a three agent system over two time-steps end
+    # varying money for a three agent system over one time-step start
     (
         [[0],[0.5],[0.2]],
         [[2],[4],[5]],
@@ -64,17 +76,31 @@ import VIAYN.utils as U
         [2,3,4],
         [(0.8/2.1)+((0.9*4)/2.1)+(4./2.1)]
     ),
+    # varying money for a three agent system over one time-step end
+    # varying money for a three agent system over two time-steps start
     (
         [[0.2,0.2],[0.3,0.3],[0.2,0.2]],
         [[2,2],[4,4],[5,5]],
         [2,3,4],
         [(0.8/2.1)+((0.9*4)/2.1)+(4./2.1), (0.8/2.1)+((0.9*4)/2.1)+(4./2.1)]
     )
+    # varying money for a three agent system over two time-steps end
 ])
 def test_simple_policy_config_single_action(
     bets,predictions,
     moneys,expected,
     gen_policy_conf,gen_weighted_bet):
+    """
+    This test checks that aggregate bets in policy config
+    satsifies our definition os simple aggregation for a 3 agent
+    system
+
+    [bets] is the bet percentage of each agent in a certain timestep
+    [predictions] is the corresponding prediction for each bet
+    [moneys] is a list of the agents money at that time-step
+    The other two parameters are test fixtures to help create
+    objects easier.
+    """
     policyConf = gen_policy_conf(fac.PolicyConfigEnum.simple)
     weightedBets : List[P.WeightedBet] = []
     for i in range(len(bets)):
@@ -88,6 +114,7 @@ def test_simple_policy_config_single_action(
     # adding sum was easier than changing expected to be a float
 
 @pytest.mark.parametrize("bets,predictions,moneys,expected", [
+    # varying bets and predictions for a three agent system over one time-step start
     (
         [[0],[0.5],[0.2]],
         [[2],[4],[5]],
@@ -106,6 +133,8 @@ def test_simple_policy_config_single_action(
         [2,2,2],
         [(0.8/1.4)+((0.6*4)/1.4)+(2./1.4)]
     ),
+    # varying bets and predictions for a three agent system over one time-step end
+    # varying bets and predictions for a three agent system over two time-steps start
     (
         [[0.2,0.2],[0.3,0.3],[0.2,0.2]],
         [[2,2],[4,4],[5,5]],
@@ -118,6 +147,8 @@ def test_simple_policy_config_single_action(
         [2,2,2],
         [(0.8/1.4)+((0.6*4)/1.4)+(2./1.4), (0.8/1.4)+((0.6*4)/1.4)+(2./1.4)]
     ),
+    # varying bets and predictions for a three agent system over two time-steps end
+    # varying money for a three agent system over one time-step start
     (
         [[0],[0.5],[0.2]],
         [[2],[4],[5]],
@@ -136,17 +167,32 @@ def test_simple_policy_config_single_action(
         [2,3,4],
         [(0.8/2.1)+((0.9*4)/2.1)+(4./2.1)]
     ),
+    # varying money for a three agent system over one time-step end
+    # varying money for a three agent system over two time-steps start
     (
         [[0.2,0.2],[0.3,0.3],[0.2,0.2]],
         [[2,2],[4,4],[5,5]],
         [2,3,4],
         [(0.8/2.1)+((0.9*4)/2.1)+(4./2.1), (0.8/2.1)+((0.9*4)/2.1)+(4./2.1)]
     )
+    # varying money for a three agent system over two time-steps end
 ])
 def test_simple_policy_config_multiple_actions(
     bets,predictions,
     moneys,expected,
     gen_policy_conf,gen_weighted_bet):
+    """
+    This test checks that aggregate bets in policy config
+    satsifies our definition of simple aggregation for a 3 agent
+    system where the agents have multiple IDENTICAL actions
+
+    [bets] is the bet percentage of each agent in a certain timestep
+    [predictions] is the corresponding prediction for each bet
+    [moneys] is a list of the agents money at that time-step
+    The other two parameters are test fixtures to help create
+    objects easier.
+    """
+    
     policyConf = gen_policy_conf(fac.PolicyConfigEnum.simple)
     weightedBets : List[P.WeightedBet] = []
     for i in range(len(bets)):
@@ -161,29 +207,48 @@ def test_simple_policy_config_multiple_actions(
 
 
 @pytest.mark.parametrize("arr,vals,expected", [
-    ([0,1,2,3,4,5],[0,0,0,0,0,100],5),
-    ([5,4,3,2,1,0],[0,0,0,0,0,100],0),
-    ([0,1,2,3,4,5],[0,10,100,1000,10000,100000],5),
-    ([5,4,3,2,1,0],[100000,10000,1000,100,10,0],5),
-    ([0,1,2,3,4,5],[0,10,100,10000000,100, 10],3),
-    ([1,0,2,3,4,5],[0,0,0,0,0,0],1),
+    ([0,1,2,3,4,5],[0,0,0,0,0,100],5), # best action satisfies highest index
+    ([5,4,3,2,1,0],[0,0,0,0,0,100],0), # best action is the lowest index
+    ([0,1,2,3,4,5],[0,10,100,1000,10000,100000],5), # actions have varying values
+    ([5,4,3,2,1,0],[100000,10000,1000,100,10,0],5), # actions with varying values are not in order
+    ([0,1,2,3,4,5],[0,10,100,10000000,100, 10],3), # best action is the in middle
+    # ([1,0,2,3,4,5],[0,0,0,0,0,0],1), # TODO: all actions have the same value should be tested
     # ([],[],None), # TODO: should test that this throws
 ])
 def test_simple_policy_config_select_action(arr,vals,expected,gen_policy_conf):
+    """
+    This test checks that select action in policy config
+    satsifies our definition of simple select action
+
+    [arr] is the list of actions available
+    [vals] is the list of values for each action in [arr]
+    The other parameter is a test fixture to help create
+    objects easier.
+    """
     policyConf = gen_policy_conf(fac.PolicyConfigEnum.simple)
     res = policyConf.select_action({key:val for key,val in zip(arr,vals)})
     assert(res == expected)
 
 @pytest.mark.parametrize("arr,vals,expected", [
-    ([0,1,2,3,4,5],[0,0,0,0,0,100],5),
-    ([5,4,3,2,1,0],[0,0,0,0,0,100],0),
-    ([0,1,2,3,4,5],[0,10,100,1000,10000,100000],5),
-    ([5,4,3,2,1,0],[100000,10000,1000,100,10,0],5),
-    ([0,1,2,3,4,5],[0,10,100,10000000,100, 10],3),
-    ([1,0,2,3,4,5],[0,0,0,0,0,0],1),
+    ([0,1,2,3,4,5],[0,0,0,0,0,100],5), # best action satisfies highest index
+    ([5,4,3,2,1,0],[0,0,0,0,0,100],0), # best action is the lowest index
+    ([0,1,2,3,4,5],[0,10,100,1000,10000,100000],5), # actions have varying values
+    ([5,4,3,2,1,0],[100000,10000,1000,100,10,0],5), # actions with varying values are not in order
+    ([0,1,2,3,4,5],[0,10,100,10000000,100, 10],3), # best action is the in middle
+    # ([1,0,2,3,4,5],[0,0,0,0,0,0],1), # TODO: all actions have the same value should be tested
     # ([],[],None), # TODO: should test that this throws
 ])
 def test_simple_policy_config_action_probs(arr,vals,expected,gen_policy_conf):
+    """
+    This test checks that action probs in policy config
+    satsifies our definition of simple action probabilities which is identical
+    to select action but gives out probabilities.
+
+    [arr] is the list of actions available
+    [vals] is the list of values for each action in [arr]
+    The other parameter is a test fixture to help create
+    objects easier.
+    """
     policyConf = gen_policy_conf(fac.PolicyConfigEnum.simple)
     res = policyConf.action_probabilities({key:val for key,val in zip(arr,vals)})
     for i in arr:
